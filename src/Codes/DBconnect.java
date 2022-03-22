f3f56c0ee6ea62617a54d2c7a0c587291598d33a
KavinduPiyumantha <tkp99x@gmail.com>
2022-03-22 22:19:44 +0530

    book copy update feature done

---
+++ src/Codes/DBconnect.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Codes;
+
+import Interfaces.Login;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author KAVINDU PIYUMANTHA
+ */
+public class DBconnect {
+    
+        
+    
+        public static Connection Connect(){
+            
+            Connection con= null ;
+            
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Library","root","Silvatkp99");               
+            
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null  ,ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null  ,ex);
+        }
+        return con;
+    }
+    
+    
+}
